26fc89d27fa8ec889add7accd1deaafc5ba43964
Matthew Weier O'Phinney <matthew@zend.com>
2018-02-21 16:14:50 -0600

    Extend package-specific RuntimeException
    Since it exists...

--- src/Exception/CircularReferenceException.php
+++ src/Exception/CircularReferenceException.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 
 namespace Zend\Permissions\Rbac\Exception;
 
-class CircularReferenceException extends \RuntimeException implements
-    ExceptionInterface
+class CircularReferenceException extends RuntimeException implements ExceptionInterface
 {
 }
